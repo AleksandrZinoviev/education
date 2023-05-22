58bf04beb14367e5d8397d1cb30b795f82f32a81
AleksandrZinoviev <84877683+AleksandrZinoviev@users.noreply.github.com>
2023-05-22 21:00:36 +0300

    Create task_1.js

---
+++ JavaScript/13929/task_1.js
@@ -0,0 +1,9 @@
+const pi = 3.14;
+
+// BEGIN (write your solution here)
+const weight = 65
+const c = 300000000
+const energy = weight * (c**2)
+// END
+
+console.info(`Your body energy is ${energy} joules. This is 1 / ${Math.round(389000000000000000000 / energy)} of the yearly world energy consumption. Woah!`);
